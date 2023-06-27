46980decbab86a03ed3fac614bff2a655dfc5ae2
Shonu72 <shourya7250@gmail.com>
2023-06-27 16:09:35 +0530

    final keyword

---
+++ src/com/shonu/finalKeyword.java
@@ -0,0 +1,27 @@
+package com.shonu;
+class A{
+    final int no0fAlphabets = 26; //Final Variables - Value once assigned cannot be changed
+    final int noOfDigits;
+    //Either assign the value at time of declaration or assign the value inside constructors
+    A(){
+        noOfDigits = 10;
+    }
+    final void fun(){
+        System.out.println("Hi, I'm a function in class A");
+    }
+
+        final class B extends A {   //Final classes cannot be inherited
+        }
+    }
+
+
+// void fun()
+// C
+// }
+//System.out.println("I'm function in B overriding function in A");
+            //class C extends B(
+public class finalKeyword {
+                public static void main(String[] args) {
+
+                }
+        }
